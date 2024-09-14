26af43c287fe82946d8191a2ca7cfc15f65ef07a
Andrey Dolganov <dolganov2002@mail.ru>
2024-09-15 00:50:37 +0300

    update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,9 +2,9 @@ from gendiff.generate_diff.generate_diff import gen_diff
 
 
 def test_gen_diff():
-    one = 'fixtures/one.json'
-    two = 'fixtures/two.json'
-    with_links_path = 'fixtures/result.txt'
+    one = 'tests/fixtures/one.json'
+    two = 'tests/fixtures/two.json'
+    with_links_path = 'tests/fixtures/result.txt'
     with open(with_links_path, encoding='utf8') as f:
         result = f.read()
     assert gen_diff(one, two) == result
