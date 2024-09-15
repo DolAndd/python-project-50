57dca159f8c9f11dd904f5444c2a6a544731185f
Andrey Dolganov <dolganov2002@mail.ru>
2024-09-15 21:40:04 +0300

    update generate_diff.py complex

--- gendiff/generate_diff/generate_diff.py
+++ gendiff/generate_diff/generate_diff.py
@@ -27,14 +27,10 @@ def gen_diff(pathfile_1, pathfile_2):
     for i in file_1:
         if i in file_2 and file_1[i] == file_2[i]:
             result[f'  {i}'] = convert_bool(file_1[i])
-        elif i in file_2 and file_1[i] != file_2[i]:
-            result[f'- {i}'] = convert_bool(file_1[i])
         else:
             result[f'- {i}'] = convert_bool(file_1[i])
     for i in file_2:
-        if i in file_1 and file_2[i] != file_1[i]:
-            result[f'+ {i}'] = convert_bool(file_2[i])
-        elif i not in file_1:
+        if (i in file_1 and file_2[i] != file_1[i]) or (i not in file_1):
             result[f'+ {i}'] = convert_bool(file_2[i])
     result_dict = dict(sorted(result.items(), key=lambda x: x[0][2:]))
     return '{\n' + convert_dict_to_str(result_dict) + '}'
